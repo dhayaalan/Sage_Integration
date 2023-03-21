4c72e3e77b2266ff6ce824168f771476e9b8eb0f
dhayaalan <dhayaalanjayakumar2001@gmail.com>
2023-03-21 13:20:45 +0530

    Changes And Working of checkbox

--- config/assets/iparams.js
+++ config/assets/iparams.js
@@ -1,12 +1,25 @@
-function checkHandler(){
-    // const checkbox = document.getElementsByClassName('myCheckbox');
-    //check-box
-    const checkbox = document.getElementById('check-box');
+function checkHandler() {
+  // const checkbox = document.getElementsByClassName('myCheckbox');
+  //check-box
+  const checkbox = document.getElementById('check-box1');
 
-    console.log(!checkbox.checked, 'checkbox');
-    if (!checkbox.checked == true) {
-      console.log('success');
-    } else {
-      console.log('error');
-    }
+  console.log('checkbox');
+  if (!checkbox.checked == true) {
+    console.log('success');
+  } else {
+    console.log('error');
+  }
+}
+
+function checkHandle() {
+  // const checkbox = document.getElementsByClassName('myCheckbox');
+  //check-box
+  const checkbox = document.getElementById('check-box2');
+
+  console.log('checkbox');
+  if (!checkbox.checked == true) {
+    console.log('success');
+  } else {
+    console.log('error');
+  }
 }
